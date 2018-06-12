176d912ff6d493a96b64d4b6a04941630000c284
Andrii Vasyliev <sol@hiqdev.com>
2018-06-12 22:12:34 +0300

    removed obsolete `nvim` installation command from zsh

--- .config/zsh/functions.sh
+++ .config/zsh/functions.sh
@@ -107,19 +107,6 @@ ydl() {
     $file $@
 }
 
-nvim() {
-    file="/usr/bin/nvim"
-
-    if [ ! -e "$file" ]; then
-        sudo add-apt-repository ppa:neovim-ppa/stable
-        sudo apt-get update
-        sudo apt-get install neovim
-        sudo apt-get install python-dev python-pip python3-dev python3-pip
-    fi
-
-    $file $@
-}
-
 bashinto() {
     docker exec -it $1 bash -c "stty cols $COLUMNS rows $LINES && bash";
 }
