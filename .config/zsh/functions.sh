d1aba878b949087f09bf21c41bd70fefdd350456
Andrii Vasyliev <sol@hiqdev.com>
2019-03-13 22:01:52 +0200

    Updated `install_ripgrep` to latest ripgrep version 0.10

--- .config/zsh/functions.sh
+++ .config/zsh/functions.sh
@@ -172,6 +172,6 @@ path() {
 }
 
 install_ripgrep() {
-    curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.8.1/ripgrep_0.8.1_amd64.deb
-    sudo dpkg -i ripgrep_0.8.1_amd64.deb && rm ripgrep_0.8.1_amd64.deb
+    curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.10.0/ripgrep_0.10.0_amd64.deb
+    sudo dpkg -i ripgrep_0.10.0_amd64.deb && rm ripgrep_0.10.0_amd64.deb
 }
