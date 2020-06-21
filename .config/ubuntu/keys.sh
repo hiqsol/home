f78aefd1e115ea07a94f1ff5204874e86c0d06f3
Andrii Vasyliev <sol@hiqdev.com>
2020-06-21 23:15:45 +0300

    Add ubuntu keybinding config

---
+++ .config/ubuntu/keys.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+GSETKEY="gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings"
+
+KEY_PATH="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings"
+
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings \
+"['$KEY_PATH/custom0/', '$KEY_PATH/custom1/', '$KEY_PATH/custom2/', '$KEY_PATH/custom3/', '$KEY_PATH/custom4/', '$KEY_PATH/custom5/']"
+
+$GSETKEY/custom0/ name "KeePassXC"
+$GSETKEY/custom0/ command "keepassxc"
+$GSETKEY/custom0/ binding "<Super>K"
+
+$GSETKEY/custom1/ name "Kill all browsers"
+$GSETKEY/custom1/ command "pkill -9 'chromium|chrome|firefox'"
+$GSETKEY/custom1/ binding "<Super>B"
+
+$GSETKEY/custom4/ name "Google Chrome"
+$GSETKEY/custom4/ command "google-chrome"
+$GSETKEY/custom4/ binding "<Super>C"
+
+$GSETKEY/custom5/ name "Firefox"
+$GSETKEY/custom5/ command "firefox"
+$GSETKEY/custom5/ binding "<Super>F"
+
+$GSETKEY/custom2/ name "Screenshot Selection"
+$GSETKEY/custom2/ command "gnome-screenshot --area"
+$GSETKEY/custom2/ binding "<Shift><Super>Print"
+
+$GSETKEY/custom3/ name "Gnome Terminal"
+$GSETKEY/custom3/ command "x-terminal-emulator"
+$GSETKEY/custom3/ binding "<Ctrl><Alt>T"
+
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>3']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>4']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5 "['<Super>5']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 "['<Super>6']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-7 "['<Super>q']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-8 "['<Super>w']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-9 "['<Super>e']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-10 "['<Super>r']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-11 "['<Super>t']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-12 "['<Super>"'`'"']"
+
+# Overview Workspaces
+gsettings --schemadir ~/.local/share/gnome-shell/extensions/wsmatrix@martin.zurowietz.de/schemas/ set org.gnome.shell.extensions.wsmatrix-keybindings workspace-overview-toggle "['<Super><Ctrl>W']"
+
