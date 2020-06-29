b59d5e06f8b476e31e4637efcd915cb00f6fd3ce
Andrii Vasyliev <sol@hiqdev.com>
2020-06-29 23:44:53 +0300

    Fix ubuntu keybinding to switch to workspace 12

--- .config/ubuntu/keys.sh
+++ .config/ubuntu/keys.sh
@@ -42,6 +42,8 @@ gsettings set org.gnome.shell.keybindings switch-to-application-7 "['<Super>F7']
 gsettings set org.gnome.shell.keybindings switch-to-application-8 "['<Super>F8']"
 gsettings set org.gnome.shell.keybindings switch-to-application-9 "['<Super>F9']"
 
+gsettings set org.gnome.desktop.wm.keybindings switch-group "[]"
+
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>3']"
@@ -49,11 +51,11 @@ gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>4
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5 "['<Super>5']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 "['<Super>6']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-7 "['<Super>q']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-8 "['<Super>w']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-9 "['<Super>e']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-10 "['<Super>r']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-11 "['<Super>t']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-12 "['<Super>"'`'"']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-8 "['<Super>W']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-9 "['<Super>E']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-10 "['<Super>R']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-11 "['<Super>T']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-12 "['<Super>Y','<Super>Grave']"
 
 # Overview Workspaces
 gsettings --schemadir ~/.local/share/gnome-shell/extensions/wsmatrix@martin.zurowietz.de/schemas/ set org.gnome.shell.extensions.wsmatrix-keybindings workspace-overview-toggle "['<Super><Ctrl>W']"
