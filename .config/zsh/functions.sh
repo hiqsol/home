a80eda67005b5445dea96c3202b74fb754783167
Andrii Vasyliev <sol@hiqdev.com>
2020-07-17 16:15:22 +0000

    Rename and improve `cdv` shell alias to go to the vendor dir

--- .config/zsh/functions.sh
+++ .config/zsh/functions.sh
@@ -70,8 +70,14 @@ cdvp() {
 }
 
 # cd to /home/user/prj/organization/project/vendor/ORGANIZATION
-cdvo() {
+cdv() {
     pts=(${(s:/:)PWD})
+    if [[ -z $pts[7] ]]; then
+        pts[7]='hiqdev'
+    fi
+    if [[ -z $pts[6] ]]; then
+        pts[6]='vendor'
+    fi
     cd "/$pts[1]/$pts[2]/$pts[3]/$pts[4]/$pts[5]/$pts[6]/$pts[7]"
 }
 
