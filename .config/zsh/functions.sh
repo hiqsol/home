aa2fe6dc274e22880a17513540b9fbc8ba245fad
Andrii Vasyliev <sol@hiqdev.com>
2020-12-02 11:20:05 +0200

    Remove obsolete install scripts

--- .config/zsh/functions.sh
+++ .config/zsh/functions.sh
@@ -230,18 +230,6 @@ vim8() {
     $file $@
 }
 
-keepassxc() {
-    file="$HOME/bin/keepassxc"
-
-    if [ ! -x $file ]; then
-        url=`curl --silent "https://api.github.com/repos/keepassxreboot/keepassxc/releases/latest" | jq -r '.assets[].browser_download_url | select (. | test("-x86_64.AppImage$"))'`
-        curl -L $url -o $file
-        chmod a+x $file
-    fi
-
-    $file $@
-}
-
 hub() {
     file="/usr/local/bin/hub"
 
@@ -265,11 +253,6 @@ install_hub() {
 # also consider `realpath`
 path() {
     echo "$("cd" "$(dirname "$1")" && pwd)/$(basename "$1")"
-}
-
-install_ripgrep() {
-    curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.10.0/ripgrep_0.10.0_amd64.deb
-    sudo dpkg -i ripgrep_0.10.0_amd64.deb && rm ripgrep_0.10.0_amd64.deb
 }
 
 get_php_version() {
